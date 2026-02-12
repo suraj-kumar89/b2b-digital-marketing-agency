8ad30b4080c8ac60f11016a03e5873bcb2209d9c
suraj-kumar89 <suraj@bluvodigital.com>
2026-02-12 16:18:14 +0530

    Update UTM

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import { Header } from '@/_components'
 import './globals.css'
 import Footer from '@/_components/footer/Footer'
 import WhatsAppButton from '@/_components/WhatsApp'
+import Script from "next/script";
+
 export const runtime = "nodejs";
 
 export const metadata = {
@@ -22,20 +24,43 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        {/* Fonts */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
           href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:ital,wght@0,200..800;1,200..800&display=swap"
           rel="stylesheet"
         />
-        {/* GTM script stays */}
+
+        {/* ✅ Google Tag Manager Script */}
+        <Script id="gtm-script" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-M2HQ4L2K');
+          `}
+        </Script>
       </head>
+
       <body>
+        {/* ✅ GTM Noscript */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-M2HQ4L2K"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+
         <div className="flex flex-col flex-1 relative">
           <Header />
           <div className="flex-1">{children}</div>
           <Footer />
         </div>
+
         <WhatsAppButton />
       </body>
     </html>
